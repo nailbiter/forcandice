61c611af6190ea5efbd468480e60a60fcdd8ecdd
Alex Leontiev <inp9822058@alumni.cs.nctu.edu.tw>
2018-06-06 17:41:45 +0800

    json

--- test.pl
+++ test.pl
@@ -1,6 +1,12 @@
 #!/usr/local/bin/perl
 # hello.pl - My first CGI program
 
+use strict;
+use warnings;
+
+use lib qw(/u/cs/98/9822058/lib/perl5/site_perl/);
+use JSON;
+
 print "Content-Type: text/html\n\n";
 # Note there is a newline between 
 # this header and Data
@@ -12,4 +18,21 @@ print "<title>Hello, world!</title>";
 print "</head>\n";
 print "<body>\n";
 print "<h1>Hello, world!</h1>\n";
+
+my $filename = '/u/cs/98/9822058/assistantBotFiles/assistantBotFiles/time.json';
+
+my $json_text = do {
+   open(my $json_fh, "<:encoding(UTF-8)", $filename)
+      or die("Can't open \$filename\": $!\n");
+   local $/;
+   <$json_fh>
+};
+
+my $json = JSON->new;
+my $data = $json->decode($json_text);
+
+for ( @{$data->{arr}} ) {
+   print $_->{name}."\n";
+}
+
 print "</body> </html>\n";
